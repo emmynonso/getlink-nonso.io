9e5e17da580e0bfbdd3fdb1b6810cf9040526852
Emmanuel Chinonso <emmanuelchinonso1340@gmail.com>
2023-10-21 07:56:20 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -5,7 +5,10 @@
         navLinks.classList.toggle("top-[10%]");
       }
 
-    
+
+
+      // submit form
+
       function submitNewForm() {
         const name = document.getElementById("names").value;
         const email = document.getElementById("emails").value;
@@ -17,7 +20,7 @@
         }
       }
 
-
+      // submit form
 
       function submitForm() {
         const name = document.getElementById("name").value;
@@ -28,3 +31,33 @@
         }
       }
 
+
+      function updateCountdown() {
+        const targetTime = new Date("01-10-2023");
+        targetTime.setHours(targetTime.getHours() + 23);
+        targetTime.setMinutes(targetTime.getMinutes() + 59);
+        targetTime.setSeconds(targetTime.getSeconds() + 30);
+
+        const now = new Date("01-10-2023");
+        const remainingTime = targetTime - now;
+
+        const hours = Math.floor(remainingTime / (1000 * 60 * 60));
+        const minutes = Math.floor(
+          (remainingTime % (1000 * 60 * 60)) / (1000 * 60)
+        );
+        const seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
+
+        document.getElementById(
+          "countdown-timer"
+        ).textContent = `${hours}hrs ${minutes}mins ${seconds}s`;
+
+        if (remainingTime <= 0) {
+          clearInterval(countdownInterval);
+          document.getElementById("countdown-timer").textContent =
+            "Countdown finished!";
+        }
+      }
+
+      const countdownInterval = setInterval(updateCountdown, 1000);
+
+      
